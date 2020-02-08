1f1abab8e690da81d940c958a73f0012b909d414
notreniev <everton.canez@gmail.com>
2020-02-08 01:19:21 -0300

    Organização do layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnInit{
   title = 'frontend';
+  @ViewChild('collapseUtilities', { static: false }) utilities: ElementRef
+  @ViewChild('collapsePages', { static: false }) pages: ElementRef
+  @ViewChild("collapseComponents", { static: false }) componentes: ElementRef;
+
   public isCollapsedComponents = true;
   public isCollapsedUtilities = true;
   public isCollapsedPages = true;
   public isCollapsedCharts = true;
   public isMenuCollapsed = true;
+
+
+  constructor(){
+  }
+
+  ngAfterViewInit(){
+
+  }
+
+  ngOnInit(){
+  }
 
 }
