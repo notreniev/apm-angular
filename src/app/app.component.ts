f8261a475e1b3e2b92b4a1fb01b7ae4dc5957a39
notreniev <everton.canez@gmail.com>
2020-02-09 22:34:36 -0300

    Resolvido o problema com submenus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,16 @@ export class AppComponent implements AfterViewInit, OnInit{
   title = 'frontend';
 
   @ViewChild('components') 
-   private components: ElementRef;
+  private components: ElementRef;
 
-   @ViewChild('utilities') 
-   private utilities: ElementRef;
+  @ViewChild('utilities') 
+  private utilities: ElementRef;
 
-   @ViewChild('pages') 
-   private pages: ElementRef;
+  @ViewChild('pages') 
+  private pages: ElementRef;
+
+  @ViewChild('accordionSidebar')
+  private navItem: ElementRef;
 
   public isCollapsedComponents = true;
   public isCollapsedUtilities = true;
@@ -23,7 +26,7 @@ export class AppComponent implements AfterViewInit, OnInit{
   public isCollapsedCharts = true;
   public isMenuCollapsed = true;
 
-  constructor(private renderer: Renderer2){
+  constructor(private renderer: Renderer2, private elRef:ElementRef){
   }
 
   ngAfterViewInit(){
@@ -46,8 +49,30 @@ export class AppComponent implements AfterViewInit, OnInit{
       this.renderer.removeClass(this.utilities.nativeElement, 'show')
       this.renderer.addClass(this.pages.nativeElement, 'show')
     });
-
   }
+
+/**
+ * 
+ *     // const navItems = this.elRef.nativeElement.querySelectorAll('li')
+    // navItems.forEach(element => {
+    //   const div = element.querySelector('div')
+    //   if (div){
+    //     const show = div.classList.contains('collapse')
+    //     if (show){
+    //       console.log(div)
+    //       this.renderer.listen(this.components.nativeElement, 'click', () => {
+    //         this.renderer.removeClass(this.utilities.nativeElement, 'show')
+    //         this.renderer.removeClass(this.pages.nativeElement, 'show')      
+    //         this.renderer.addClass(this.components.nativeElement, 'show')
+    //         console.log('this1', div)
+    //       })    
+    //     }
+    //   }
+    // });
+
+ * 
+ * 
+ */
 
   ngOnInit(){
   }
