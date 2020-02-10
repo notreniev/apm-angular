35955a79e5a1f3af8e5ed684e38af65151d73cce
notreniev <everton.canez@gmail.com>
2020-02-10 17:07:03 -0300

    Testes com property binding

--- src/shared/components/content-wrapper/content-wrapper.component.ts
+++ src/shared/components/content-wrapper/content-wrapper.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ContentWrapperComponent implements OnInit {
 
-  titulo: string = 'teste2'
+  titulo: string = 'teste card'
 
   constructor() { }
 
