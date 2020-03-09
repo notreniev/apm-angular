08f3e4a12bab5995df5f12eebf7b7702ebfe02e4
notreniev <everton.canez@gmail.com>
2020-03-09 15:08:22 -0300

    Ajustes css

--- src/shared/components/card-badge/card-badge.component.ts
+++ src/shared/components/card-badge/card-badge.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'apm-card-badge',
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CardBadgeComponent implements OnInit {
 
-  @Input() color: any
+  @Input() color: any;
 
   constructor() { }
 
